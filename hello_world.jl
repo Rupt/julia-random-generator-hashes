c16236b6e9883395f0613b86c8f2af68c0cb803e
Rupert Tombs <rupert.tombs@gmail.com>
2023-11-03 13:28:03 +0000

    Give up on abstract interfaces

--- hello_world.jl
+++ hello_world.jl
@@ -2,26 +2,21 @@ module BitSift
 
 using Printf
 
-# Abstract generator interface
-abstract type AbstractRNG end
+struct SplitMix64 end
 
-function hash(context::AbstractRNG, key::UInt64)::UInt64
-    return hash_impl(context, key);
-end
-
-# An example implementation
-struct SplitMix64 <: AbstractRNG end
-
-function hash_impl(context::SplitMix64, key::UInt64)::UInt64
-    x = key * 0x9e3779b97f4a7c15;
-    x = xor(x, x >> 30) * 0xbf58476d1ce4e5b9;
-    x = xor(x, x >> 27) * 0x94d049bb133111eb;
-    return xor(x, x >> 31);
+function hash(::SplitMix64, key::UInt64)::UInt64
+    x = key * 0x9e3779b97f4a7c15
+    x = xor(x, x >> 30) * 0xbf58476d1ce4e5b9
+    x = xor(x, x >> 27) * 0x94d049bb133111eb
+    return xor(x, x >> 31)
 end
 
 
 for i in 0:10
     Printf.@printf "%#018x: %#018x\n" i hash(SplitMix64(), UInt64(i))
 end
+
+
+Printf.@printf "%#018x: %#018x\n" 3 hash(AbstractRNG(), UInt64(3))
 
 end  # module BitSift
