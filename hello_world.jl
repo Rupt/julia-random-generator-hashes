eaf0bfa01aed2cd9e389e110da9220158b43c546
Rupert Tombs <rupert.tombs@gmail.com>
2023-11-04 10:09:51 +0000

    Add an abstract interface with some functions; play with op.

--- hello_world.jl
+++ hello_world.jl
@@ -2,19 +2,35 @@ module BitSift
 
 import Printf
 
-struct SplitMix64 end
+# Abstract interface
+abstract type AbstractRNG end
 
-function hash(::SplitMix64, key::UInt64)::UInt64
+function query end
+function bits end
+
+
+# An example implementation
+struct SplitMix64 <: AbstractRNG end
+
+function query(::SplitMix64, key::UInt64)::UInt64
     x = key * 0x9e3779b97f4a7c15
     x = xor(x, x >> 30) * 0xbf58476d1ce4e5b9
     x = xor(x, x >> 27) * 0x94d049bb133111eb
     return xor(x, x >> 31)
 end
 
+function bits(::SplitMix64)::BitVector
+    return BitVector()
+end
+
 function main()
     for i in 0:10
-        Printf.@printf "%#018x: %#018x\n" i hash(SplitMix64(), UInt64(i))
+        Printf.@printf "%#018x: %#018x\n" i query(SplitMix64(), UInt64(i))
     end
+    querty_splitmix64(k) = query(SplitMix64(), UInt64(k))
+    println(querty_splitmix64.(0:3))
+    println((k -> query(SplitMix64(), UInt64(k))).(0:3))
+    println(bits(SplitMix64()))
 end
 
 @time main()
