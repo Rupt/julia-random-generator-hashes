ea939c103f5f8544c4f8a11976ab365f4a9939c3
Rupert Tombs <rupert.tombs@gmail.com>
2023-11-05 20:47:22 +0000

    Add a MWC128 test; improve naming

--- src/MultiplyWithCarry.jl
+++ src/MultiplyWithCarry.jl
@@ -1,20 +1,21 @@
 struct MultiplyWithCarry64 <: AbstractRNG
     modulus::UInt128
-    seed::UInt128
+    seed_c::UInt64
+    seed_x::UInt64
 end
 
 function query(rng::MultiplyWithCarry64, key::UInt64)::UInt64
-    p::UInt128 = rng.modulus
-    a::BigInt = invmod(UInt128(1) << 64, p)
-    x::BigInt = rng.seed
+    m::UInt128 = rng.modulus
+    a::BigInt = invmod(UInt128(1) << 64, m)
+    x::BigInt = (UInt128(rng.seed_c) << 64) | rng.seed_x
     for i in 0:63
-        x = Bool((key >> i) & 1) ? (a * x) % p : x
-        # Two steps: a * ((a * x) % p) % p = ((a * a) % p * x) % p
-        a = (a * a) % p
+        x = Bool((key >> i) & 1) ? (a * x) % m : x
+        # Two steps: a * ((a * x) % m) = ((a * a) % m) * x
+        a = (a * a) % m
     end
     return x & typemax(UInt64)
 end
 
 function encode(rng::MultiplyWithCarry64)::BitVector
-    return cat(encode(rng.multiplier), encode(rng.seed); dims=1)
+    return cat(encode(rng.modulus), encode(rng.seed_c), encode(rng.seed_x); dims=1)
 end
--- test/MultiplyWithCarry.jl
+++ test/MultiplyWithCarry.jl
@@ -1,8 +1,8 @@
 using Printf
 function _ref_kiss_mwc(key::UInt64)::UInt64
-    # // https://web.archive.org/web/20221206094913/https://www.thecodingforums.com/threads/64-bit-kiss-rngs.673657/
-    x::UInt64 = 1234567890987654321
+    # https://web.archive.org/web/20221206094913/https://www.thecodingforums.com/threads/64-bit-kiss-rngs.673657/
     c::UInt64 = 123456123456123456
+    x::UInt64 = 1234567890987654321
     for _ in 1:key
         t::UInt64 = (x << 58) + c
         c = x >> 6
@@ -12,83 +12,39 @@ function _ref_kiss_mwc(key::UInt64)::UInt64
     return x
 end
 
-function _ref_kiss_mwc_dumb(key::UInt64)::UInt64
-    x::UInt64 = 1234567890987654321
-    c::UInt64 = 123456123456123456
-    a::UInt128 = UInt128(1) << 58 + 1
-    b::UInt128 = UInt128(1) << 64
-    p::UInt128 = a * b - 1
-    b_inverse::UInt128 = invmod(b, p)
-    s::BigInt = x + c * b
-    for _ in 1:key
-        s = (b_inverse * s) % p
-    end
-    return s & typemax(UInt64)
-end
-
 function _ref_kiss_mwc_rng(key::UInt64)::UInt64
-    x::UInt64 = 1234567890987654321
     c::UInt64 = 123456123456123456
+    x::UInt64 = 1234567890987654321
     a::UInt64 = UInt64(1) << 58 + 1
     modulus::UInt128 = (UInt128(a) << 64) - 1  # prime
-    rng = MultiplyWithCarry64(modulus, (UInt128(c) << 64) | x)
+    rng = MultiplyWithCarry64(modulus, c, x)
     return query(rng, key)
 end
 
-function _mwc_demo()
-    c = UInt64(1)
-    x = UInt64(0)
-    a = 7
-    b = 10
-    for i in 0:5
-        tmp = a * x + c
-        x = tmp % b
-        c = div(tmp, b)
-    end
-end
-
-function _mwc_redo()
-    c = UInt64(1)
-    x = UInt64(0)
-    a = 7
-    b = 10
-    p = a * b - 1
-    multiplier = invmod(b, p)
-    s = x + c * b
-    for i in 0:5
-        tmp = (multiplier * s) % p
-        x = tmp % b
-        c = div(tmp, b)
-        s = tmp
-    end
-end
-
-@testset "MultiplyWithCarry._ref_kiss_mwc" begin
+@testset "BitSift.MultiplyWithCarry" begin
+    modulus::UInt128 = 0xffebb71d94fcdaf8ffffffffffffffff
+    seed_c::UInt64 = 1
+    seed_x::UInt64 = 3141592653589793238
+    rng = MultiplyWithCarry64(modulus, seed_c, seed_x)
+    # query
+    @test typeof(query(rng, UInt64(0))) === UInt64
     # Reference: https://godbolt.org/z/fezcd1rqM
-    @test _ref_kiss_mwc(UInt64(0)) === 0x112210f4b16c1cb1
-    @test _ref_kiss_mwc(UInt64(1)) === 0xd6d8aba5615f0ef1
-    @test _ref_kiss_mwc(UInt64(2)) === 0x9b1d33e93424bf63
-    @test _ref_kiss_mwc(UInt64(3)) === 0x2a789697c9aa3b9f
-
+    @test _ref_kiss_mwc_rng(UInt64(0)) === 0x112210f4b16c1cb1
+    @test _ref_kiss_mwc_rng(UInt64(1)) === 0xd6d8aba5615f0ef1
+    @test _ref_kiss_mwc_rng(UInt64(2)) === 0x9b1d33e93424bf63
+    @test _ref_kiss_mwc_rng(UInt64(3)) === 0x2a789697c9aa3b9f
     for i in UInt64.(0:8)
-        @test _ref_kiss_mwc(i) === _ref_kiss_mwc_dumb(i)
+        @test _ref_kiss_mwc(i) === _ref_kiss_mwc_rng(i)
     end
-
-    # println(_ref_kiss_mwc(UInt64(0)))
-    # println(_ref_kiss_mwc_dumb(UInt64(0)))
-    # println(_ref_kiss_mwc_rng(UInt64(0)))
-    # println(_ref_kiss_mwc(UInt64(1)))
-    # println(_ref_kiss_mwc_dumb(UInt64(1)))
-    # println(_ref_kiss_mwc_rng(UInt64(1)))
+    # Reference: https://godbolt.org/z/WTx4P8fnv
+    @test query(rng, UInt64(0)) === 0x2b992ddfa23249d6
+    @test query(rng, UInt64(1)) === 0x8ee32211fc720d27
+    @test query(rng, UInt64(2)) === 0x8d4afc4090e4aa85
+    @test query(rng, UInt64(3)) === 0x5dcc5d9277c19e28
+    # encode
+    code = encode(rng)
+    @test length(code) === 256
+    @test code[1:128] == encode(modulus)
+    @test code[129:192] == encode(seed_c)
+    @test code[193:256] == encode(seed_x)
 end
-
-# @testset "BitSift.MultiplyWithCarry" begin
-#     rng = MultiplyWithCarry((UInt128(1) << 58) + 1)
-#     # query
-#     @test typeof(query(rng, UInt64(0))) === UInt64
-#     for i in UInt64.(0:8)
-#         @test query(rng, i) === _ref_kiss_mwc(UInt64(i))
-#     end
-#     # encode
-#     @test encode(SplitMix64()) == Bool[]
-# end
