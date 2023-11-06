cd3e88414bd1a4559816a11096a56b6798b0664a
Rupert Tombs <rupert.tombs@gmail.com>
2023-11-06 19:57:04 +0000

    Remove a fixme with no prospect of fixing

--- src/tools.jl
+++ src/tools.jl
@@ -1,8 +1,5 @@
 function encode(key::T)::BitVector where {T<:Integer}
     length = sizeof(typemax(T) | typemin(T)) * 8  # Valid types must have bounds.
-    # FIXME: this should be a trivial copy for primitive types, but it compiles
-    # to lengthy code involving `j_fill_bitarray_from_itr`.
-    # https://godbolt.org/z/n5Tx3W4Pf
     return BitVector((key >> i) & 1 for i in (length - 1):-1:0)
 end
 
