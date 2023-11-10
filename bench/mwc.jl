79615f89ee4989faea90dc76a7469cc5410ce3ab
Rupert Tombs <rupert.tombs@gmail.com>
2023-11-10 19:34:54 +0000

    Standardize whitespace

--- bench/mwc.jl
+++ bench/mwc.jl
@@ -5,7 +5,6 @@ mwc_a1::UInt64 = 0xffebb71d94fcdaf9
 seed_c::UInt64 = 1
 seed_x::UInt64 = 3141592653589793238
 rng = MultiplyWithCarry(mwc_a1, seed_c, seed_x)
-
 key = 0x8ee32211fc720d27
 
 for _ in 1:100  # warmup
