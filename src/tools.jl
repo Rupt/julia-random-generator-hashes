29b922ba22326999ce55333db6586b6aef8d1744
Rupert Tombs <rupert.tombs@gmail.com>
2023-11-11 17:14:07 +0000

    Some syntax tweaks for clarity and .fun

--- src/tools.jl
+++ src/tools.jl
@@ -2,20 +2,21 @@ _PrimitiveSigned = Union{Int8,Int16,Int32,Int64,Int128}
 _PrimitiveUnsigned = Union{UInt8,UInt16,UInt32,UInt64,UInt128}
 _PrimitiveInteger = Union{_PrimitiveSigned,_PrimitiveUnsigned}
 
-function encode(item::T)::Vector{UInt8} where {T<:_PrimitiveInteger}
-    return [UInt8((item >> i) & 0xff) for i in (sizeof(T) * 8 - 8):-8:0]
+function encode(item::_PrimitiveInteger)::Vector{UInt8}
+    return [UInt8((item >> i) & 0xff) for i in (sizeof(item) * 8 - 8):-8:0]
 end
 
-function encode(item::T)::Vector{UInt8} where {T<:AbstractBitHash}
-    return cat((encode(getfield(item, field)) for field in fieldnames(T))...; dims=1)
+function encode(item::AbstractBitHash)::Vector{UInt8}
+    names = fieldnames(typeof(item))
+    return cat((encode(getfield(item, name)) for name in names)...; dims=1)
 end
 
 function bitcode(byte::UInt8)::BitVector
-    return @. byte & 1 << (7:-1:0) != 0
+    return byte .& (1 .<< (7:-1:0)) .!= 0
 end
 
 function bitcode(bytes::Vector{UInt8})::BitVector
-    return cat((bitcode(byte) for byte in bytes)...; dims=1)
+    return cat(bitcode.(bytes)...; dims=1)
 end
 
 # Linear algebra modulo 2
