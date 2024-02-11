ec7f3e9f3e7c34fc7b8625250dcd4d7569fc5db3
Rupert Tombs <rupert.tombs@gmail.com>
2024-02-11 12:07:34 +0000

    Add some docstrings and comments

--- src/BitSift.jl
+++ src/BitSift.jl
@@ -17,26 +17,37 @@ abstract type AbstractBitMix end
 
 function query end
 
-# Linear algebra modulo 2
+# Linear algebra modulo 2, needed use in XorShift-type generators.
+# Defined up here to allow the top-level execution we needed for performance.
 
+"(64, 64) integer mod 2 matrix, with columns represented by 64-bit integers."
 struct XorMatrix
     column::NTuple{64,UInt64}
 end
 
-function xor_mul(left::XorMatrix, vector::UInt64)::UInt64  # L @ v
+"Return the (64, 64) @ (64,) matrix-vector product modulo 2."
+function xor_mul(left::XorMatrix, vector::UInt64)::UInt64
     x::UInt64 = 0
+    # Each bit of the vector multiplies a corresponding column of the matrix.
     for i in 0:63
         x = (vector >> i) & 1 == 1 ? xor(x, left.column[i + 1]) : x
     end
     return x
 end
 
-function xor_mul(left::XorMatrix, right::XorMatrix)::XorMatrix  # L @ R
+"Return the (64, 64) @ (64, 64) matrix-matrix product modulo 2."
+function xor_mul(left::XorMatrix, right::XorMatrix)::XorMatrix
     return XorMatrix(Tuple(xor_mul(left, col) for col in right.column))
 end
 
+
 # hash functions
-# https://web.archive.org/web/20221206094913/https://www.thecodingforums.com/threads/64-bit-kiss-rngs.673657/
+
+"""
+Keep It Simple Stupid (KISS) by George Marsaglia.
+
+https://www.thecodingforums.com/threads/64-bit-kiss-rngs.673657/
+"""
 struct KISS <: AbstractBitMix end
 
 function query(::KISS, key::UInt64)::UInt64
@@ -46,8 +57,12 @@ function query(::KISS, key::UInt64)::UInt64
     return query(mwc, key) + query(xsh, key) + query(cng, key)
 end
 
-# - Seminumerical Algorithms. The Art of Computer Programming. Vol. 2 (3rd ed.)
-#     3.2.1 The Linear Congruential Method
+"""
+Linear Congruential Generator (LCG)
+
+Seminumerical Algorithms. The Art of Computer Programming. Vol. 2 (3rd ed.)
+    3.2.1 The Linear Congruential Method
+"""
 struct Linear <: AbstractBitMix
     multiplier::UInt64
     increment::UInt64
@@ -58,6 +73,9 @@ function query(rng::Linear, key::UInt64)::UInt64
     a::UInt64 = rng.multiplier
     c::UInt64 = rng.increment
     x::UInt64 = rng.seed
+    # A composition of two linear steps is another linear step, so we expand the
+    # 'N=key' steps into a decomposed form of pow(2, i) steps as given in the
+    # binary representation of 'key'.
     for i in 0:63
         x = (key >> i) & 1 == 1 ? a * x + c : x
         # Two steps: a * (a * x + c) + c = a * a * x + a * c + c
@@ -67,7 +85,11 @@ function query(rng::Linear, key::UInt64)::UInt64
     return x
 end
 
-# https://en.wikipedia.org/wiki/Multiply-with-carry_pseudorandom_number_generator
+"""
+Multiply-with-Carry (MWC)
+
+https://en.wikipedia.org/wiki/Multiply-with-carry_pseudorandom_number_generator
+"""
 struct MultiplyWithCarry <: AbstractBitMix
     reduced_multiplier::UInt64
     seed_c::UInt64
@@ -80,6 +102,8 @@ function query(rng::MultiplyWithCarry, key::UInt64)::UInt64
     m::BigInt = (BigInt(rng.reduced_multiplier) << 64) - 1
     a::BigInt = invmod(BigInt(1) << 64, m)
     x::BigInt = (BigInt(rng.seed_c) << 64) | rng.seed_x
+    # Similar to the Linear approach, except we have no additive term and must
+    # use larger integers to avoid overflow.
     for i in 0:63
         x = (key >> i) & 1 == 1 ? (a * x) % m : x
         # Two steps: a * ((a * x) % m) = ((a * a) % m) * x
@@ -88,7 +112,12 @@ function query(rng::MultiplyWithCarry, key::UInt64)::UInt64
     return x & typemax(UInt64)
 end
 
-# https://web.archive.org/web/20220617005635/https://prng.di.unimi.it/SplitMix.c
+"""
+SplitMix64
+
+This one naturally takes the form of a hash function.
+https://prng.di.unimi.it/SplitMix.c
+"""
 struct SplitMix <: AbstractBitMix end
 
 function query(::SplitMix, key::UInt64)::UInt64
@@ -98,13 +127,20 @@ function query(::SplitMix, key::UInt64)::UInt64
     return xor(x, x >> 31)
 end
 
-# https://doi.org/10.18637%2Fjss.v008.i14
+"""
+Basic Xorshift, with one chosen set of shift constants.
+
+"Xorshift RNGs" https://doi.org/10.18637%2Fjss.v008.i14
+"""
 struct XorShift <: AbstractBitMix
     seed::UInt64
 end
 
 function query(rng::XorShift, key::UInt64)::UInt64
     x::UInt64 = rng.seed
+    # The XorShift operation is a linear matrix multiplication modulo 2, so,
+    # again for the linear generators, we expand into batches of 'pow(2, i)'
+    # steps with (precomputed) matrix coefficients.
     for i in 0:63
         x = (key >> i) & 1 == 1 ? xor_mul(_XOR_SHIFT_LEFT[i + 1], x) : x
     end
@@ -118,6 +154,9 @@ function _xor_shift_kiss(x::UInt64)::UInt64
 end
 
 const _XOR_SHIFT_LEFT::NTuple{64,XorMatrix} = let
+    # We compute this constant at compile time, because the Julia compiler
+    # otherwise fails to notice that it is constant, and wastes many cycles
+    # recomputing it at runtime.
     operators = Vector{XorMatrix}(undef, 64)
     operators[1] = left = XorMatrix(Tuple(_xor_shift_kiss.(UInt64(1) .<< (0:63))))
     for i in 2:64
@@ -125,6 +164,5 @@ const _XOR_SHIFT_LEFT::NTuple{64,XorMatrix} = let
     end
     Tuple(operators)
 end
-
 
 end  # module BitSift
