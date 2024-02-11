79a7fc0f1f458dfed528c8aff1a0abb2df146a71
Rupert Tombs <rupert.tombs@gmail.com>
2024-02-11 11:35:10 +0000

    Remove vectorized benchmarks

--- bench/bytes.jl
+++ bench/bytes.jl
@@ -4,19 +4,3 @@ using BitSift
 key = 0x8ee32211fc720d27
 
 @btime bytes(key)
-
-# WIP: Is auto vectorization efficient?
-keys = [query(SplitMix(), i) for i in UInt64.(1:1000)]
-
-function bench(keys)
-    return [bytes(key) for key in keys]
-end
-for _ in 1:100  # warmup
-    bench(keys)
-end
-@btime bench(keys)
-
-for _ in 1:100  # warmup
-    bytes(keys)
-end
-@btime bytes(keys)
